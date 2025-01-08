9cc8a294c75b3182e08c561f9e772483354da6c0
arvin-ferrer <lietlaw97@gmail.com>
2025-01-08 14:37:23 +0800

    adding pointers

---
+++ addpointers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+int add(int *ptr1, int *ptr2){
+
+  int sum;
+  int sumadd;
+  sum = *ptr1 + *ptr2;
+  return sum;
+
+}
+
+
+int main(){
+  int num1;
+  int num2;
+
+  int *ptr1 = &num1;
+  int *ptr2 = &num2;
+
+  printf("Input the first number: ");
+  scanf(" %d", &num1);
+  printf("Input the second number: ");
+  scanf(" %d", &num2);
+  
+  printf("The sum of the entered numbers is %d \n", add(ptr1, ptr2));
+
+}
