25dd9a7847e275890f629e8eb7f40ec0308eda73
arvin-ferrer <lietlaw97@gmail.com>
2025-01-08 19:56:14 +0800

    arrays

--- arrays.c
+++ arrays.c
@@ -4,14 +4,16 @@
 int main(){
   
   int i;
-  float f[4];
+  double f[4];
   f[0] = 3.14159;
   f[1] = 1.41412;
   f[2] = 3.92321;
   f[3] = 4.12345;
 
   for (i = 0; i < 4; i++){
-    printf("%f\n", f[i]);
+    printf("%d: %f\n",i, f[i]);
   }
-
+  printf("size of an array: %zu\n", sizeof(f)/sizeof(double));
+  
+  
 }
