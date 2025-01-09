95ff48e87ebf9536b130e1781c823aa194b57daa
arvin-ferrer <lietlaw97@gmail.com>
2025-01-09 19:40:12 +0800

    Stringsss

--- Exers/arrays/strings.c
+++ Exers/arrays/strings.c
@@ -10,6 +10,21 @@ int ownstrlen(char *s){
   return count;
 }
 
+void copystring(char s[]){
+  int lenOfs = strlen(s);
+  char copy[lenOfs];
+
+  strcpy(copy, s);
+  printf("\nBefore modifying\n");
+  printf("Original: %s\n", s);
+  printf("Copy: %s\n", copy);
+  printf("modified\n");
+  copy[0] = 'S';
+  copy[1] = 'A';
+  printf("Original: %s\n", s); //the original didn't change as opposed to using the assignment operator
+  printf("Copy: %s\n", copy);
+}
+
 int main(){
 
   char s[]= "Hello World"; //same as s[] = "Hello World";  
@@ -19,8 +34,8 @@ int main(){
   printf("%s\n", s);
   copyS[0] = 'z';
   printf("%s\n",s); 
-
   printf("Len of string: %d \n",ownstrlen(copyS));
 
-
+  char sample[] = "AMIS";
+  copystring(sample);
 }
