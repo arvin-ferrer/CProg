21e6a8926cff034d88df71287b8bb9cc59e4816b
arvin-ferrer <lietlaw97@gmail.com>
2025-01-13 20:21:43 +0800

    structs

--- structs/structs.c
+++ structs/structs.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 
+#include <stdio.h>
 
 struct car{
   char *name;
@@ -11,11 +11,13 @@ struct car{
 int main(){
 
 
-  struct car saturn;
-  saturn.name =  "Saturn SL/2\n";
-  saturn.price = 15999.99;
-  saturn.speed = 175;
-  printf("%lf\n", saturn.price);
+  struct car saturn = {"Satus SL\2", 16000.00, 175}; //struct initializer
+  // saturn.name =  "Saturn SL/2";
+  // saturn.price = 15999.99;
+  // saturn.speed = 175;
+  printf("Name: %s\n", saturn.name);
+  printf("Price (USD): %f\n", saturn.price);
+  printf("Top speed (km): %d\n", saturn.speed);
 
 }
 
