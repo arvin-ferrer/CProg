2aeb8713cbe9f41e17150b3f8105487bf3722373
arvin-ferrer <lietlaw97@gmail.com>
2025-01-14 20:14:35 +0800

    write to file

--- file/file.c
+++ file/file.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 
+void writetoF(char *file){
+  FILE *fh_output;
+  fh_output = fopen(file, "w");
+  int c;
+  fputs("A string\n", fh_output);
+  fputs("abc\n", fh_output);
+  fputs("123\n", fh_output);
+  fclose(fh_output);
+
+}
+
 int main(){
-  
   FILE *fp;
   fp = fopen("file.txt", "r");
   
@@ -14,4 +24,5 @@ int main(){
   fclose(fp);
 
 
+  writetoF("io.txt");
 }
