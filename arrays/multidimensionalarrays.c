8cbac7b9c000344bc56052a7ccbfcf8d020beb1e
arvin-ferrer <lietlaw97@gmail.com>
2025-01-23 18:27:18 +0800

    arrays

--- arrays/multidimensionalarrays.c
+++ arrays/multidimensionalarrays.c
@@ -8,6 +8,7 @@ int main(){
     {6,7,8,9,10}
   };
 
+
   int sizerow = sizeof(a)/sizeof(a[0]);
   int sizecol = sizeof(a[0])/sizeof(int);
 
@@ -16,7 +17,7 @@ int main(){
       printf("(%d, %d) = %d\n", row, col, a[row][col]);
     }
   }
-
+  
   int b[3][3] = {[0][0]=1, [1][1]=1, [2][2]=1};
   printf("\nIdentity Matrix\n");
   for (row = 0; row < 3; row++){
--- arrays/strings.c
+++ arrays/strings.c
@@ -26,11 +26,14 @@ void copystring(char s[]){
 }
 
 int main(){
-
   char s[]= "Hello World"; //same as s[] = "Hello World";  
+  char z[] = s;
+
   int sizeOfs = strlen(s); //string initializers either char *s or s[]
   //char *s = "Hello World"  //string literals are immutable
+
   char *copyS = s; //you are copying a pointer instead of a string
+
   printf("%s\n", s);
   copyS[0] = 'z';
   printf("%s\n",s); 
