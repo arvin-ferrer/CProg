ab1a177db0772e7a4a496f995e7befb613df1b7b
arvin-ferrer <lietlaw97@gmail.com>
2025-01-28 09:02:53 +0800

    nothing

--- pointers/addpointers.c
+++ pointers/addpointers.c
@@ -9,11 +9,13 @@ int add(int *ptr1, int *ptr2){
 }
 
 int main(){
-  int num1, num2;
 
+  int array[] = {1,3,4,5,6,7};
+  int size = sizeof(array)/sizeof(int);
+  printf("(%d)\n", size);
+  int num1, num2;
   int *ptr1 = &num1;
   int *ptr2 = &num2;
-
   printf("Input the first number: ");
   scanf(" %d", &num1);
   printf("Input the second number: ");
